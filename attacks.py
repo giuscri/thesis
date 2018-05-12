4dbc5d4e32113c842ba1529fd131829b4d32eadf
Giuseppe <giuscri@gmail.com>
2018-05-12 15:03:51 +0200

    cache cleverhans FGS computational graph when possible

--- attacks.py
+++ attacks.py
@@ -9,32 +9,40 @@ import numpy as np
 
 from models import correctly_classified
 
+FGSCACHE = {} # cache of tf symbolic variables indexed by (withtarget, network, X.shape[1:], eta)
+
 def fastgradientsign(network, X, y_target=None, eta=0.15):
     """Generate adversarial examples from X to y using FGS."""
     assert y_target is None or len(y_target) == len(X)
+    withtarget = y_target is not None
 
-    cleverhans_network = cleverhans.utils_keras.KerasModelWrapper(network)
-    attack = cleverhans.attacks.FastGradientMethod(cleverhans_network)
-
-    if y_target is not None:
+    if withtarget:
         num_classes = network.output.shape.as_list()[-1]
         onehot_y_target = keras.utils.to_categorical(y_target, num_classes=num_classes)
-        examples = attack.generate(network.input, y_target=onehot_y_target, eps=eta, ord=np.inf, clip_min=0., clip_max=1.)
+
+    elementshape = X.shape[1:]
+    cached = FGSCACHE.get((withtarget, network, elementshape, eta))
+    if cached:
+        Xsym, onehot_y_targetsym, examplesym = cached
     else:
-        examples = attack.generate(network.input, eps=eta, ord=np.inf, clip_min=0., clip_max=1.)
+        cleverhans_network = cleverhans.utils_keras.KerasModelWrapper(network)
+        attack = cleverhans.attacks.FastGradientMethod(cleverhans_network)
+
+        Xsym = tf.placeholder(tf.float32, shape=network.input.shape)
+        onehot_y_targetsym = None
+        if withtarget: onehot_y_targetsym = tf.placeholder(tf.float32, shape=network.output.shape)
+
+        kwargs = { 'eps': eta, 'ord': np.inf, 'clip_min': 0., 'clip_max': 1.}
+        if withtarget: kwargs['y_target'] = onehot_y_targetsym
+        examplesym = attack.generate(Xsym, **kwargs)
+
+        FGSCACHE[(withtarget, network, elementshape, eta)] = (Xsym, onehot_y_targetsym, examplesym)
 
     session = keras.backend.get_session()
+    feed_dict = { Xsym: X }
+    if withtarget: feed_dict[onehot_y_targetsym] = onehot_y_target
 
-    var_list = map(lambda b: b.decode(), session.run(tf.report_uninitialized_variables()))
-    uninitialized_variables = []
-    for variable in tf.global_variables():
-        if variable.name.split(':')[0] in var_list:
-            uninitialized_variables.append(variable)
-    # currently the only way of collecting uninitialized
-    # variables in tf is to scan them all.
-
-    session.run(tf.variables_initializer(uninitialized_variables))
-    return session.run(examples, feed_dict={ network.input: X })
+    return session.run(examplesym, feed_dict=feed_dict)
 
 def adversarial_score(network, X_test, y_test, attack):
     """Compute adversarial score with `attack(network, X)`."""
