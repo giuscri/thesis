3583062e551c9b48c3dcfdc04f19991500422419
Giuseppe <giuscri@gmail.com>
2018-08-19 20:32:29 +0200

    rename pre-trained model to follow directory convention of dde4912

--- tests/test_attack_via_fgs.py
+++ tests/test_attack_via_fgs.py
@@ -12,8 +12,8 @@ def test_fgs_adversarial_score_when_using_reconstruction_defense(environ):
         "python",
         "bin/attack-via-fgs",
         "--model",
-        "tests/model/reconstruction/pca-filtered-model-784-components",
-        "tests/model/reconstruction/pca-filtered-model-100-components",
+        "tests/model/pca-filtered-model-784-components-reconstruction",
+        "tests/model/pca-filtered-model-100-components-reconstruction",
         "--eta",
         "0.05",
         "0.1",
@@ -26,12 +26,12 @@ def test_fgs_adversarial_score_when_using_reconstruction_defense(environ):
     assert len(result.keys()) == 2
 
     expected = np.array([22, 53, 94])
-    scoredict = result["reconstruction/pca-filtered-model-784-components"]
+    scoredict = result["pca-filtered-model-784-components-reconstruction"]
     actual = np.array([scoredict["0.05"], scoredict["0.1"], scoredict["0.2"]])
     assert np.allclose(actual, expected, atol=5)
 
     expected = np.array([17, 43, 88])
-    scoredict = result["reconstruction/pca-filtered-model-100-components"]
+    scoredict = result["pca-filtered-model-100-components-reconstruction"]
     actual = np.array([scoredict["0.05"], scoredict["0.1"], scoredict["0.2"]])
     assert np.allclose(actual, expected, atol=5)
 
@@ -41,8 +41,8 @@ def test_fgs_adversarial_score_when_using_retrain_defense(environ):
         "python",
         "bin/attack-via-fgs",
         "--model",
-        "tests/model/retraining/pca-filtered-model-784-components",
-        "tests/model/retraining/pca-filtered-model-100-components",
+        "tests/model/pca-filtered-model-784-components-retraining",
+        "tests/model/pca-filtered-model-100-components-retraining",
         "--eta",
         "0.05",
         "0.1",
@@ -55,13 +55,13 @@ def test_fgs_adversarial_score_when_using_retrain_defense(environ):
     assert len(result.keys()) == 2
 
     score_when_reconstruction = np.array([22, 53, 94])
-    scoredict = result["retraining/pca-filtered-model-784-components"]
+    scoredict = result["pca-filtered-model-784-components-retraining"]
     score_when_retraining = np.array([scoredict["0.05"], scoredict["0.1"], scoredict["0.2"]])
     tolerance = 5
     assert np.all(score_when_retraining < score_when_reconstruction + tolerance)
 
     score_when_reconstruction = np.array([17, 43, 88])
-    scoredict = result["retraining/pca-filtered-model-100-components"]
+    scoredict = result["pca-filtered-model-100-components-retraining"]
     score_when_retraining = np.array([scoredict["0.05"], scoredict["0.1"], scoredict["0.2"]])
     tolerance = 5
     assert np.all(score_when_retraining < score_when_reconstruction + tolerance)
