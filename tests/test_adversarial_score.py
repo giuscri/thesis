220f4e3f40fd435ac2afc8875ae29dfeef7c9723
Giuseppe <giuscri@gmail.com>
2018-08-24 19:03:53 +0200

    fix build

--- tests/test_adversarial_score.py
+++ tests/test_adversarial_score.py
@@ -5,7 +5,7 @@ import numpy as np
 @pytest.fixture(autouse=True)
 def prepare_and_teardown():
     os.environ["PREFIX"] = "/tmp"
-    command = ["python", "bin/train-model", "--epochs", "10", "--retraining",
+    command = ["python", "bin/train-model", "--epochs", "10",
                "--pca", "--n-components", "784"]
     subprocess.run(command)
     yield
@@ -31,30 +31,6 @@ def test_adversarial_score_when_using_reconstruction_defense(environ):
     assert len(adversarial_score_dictionary.keys()) == 3
 
     expected = np.array([0.22, 0.53, 0.93])
-    actual = np.array([adversarial_score_dictionary["0.05"],
-                       adversarial_score_dictionary["0.1"],
-                       adversarial_score_dictionary["0.2"]])
-    assert np.allclose(actual, expected, atol=0.01)
-
-
-def test_adversarial_score_when_using_retrain_defense(environ):
-    command = [
-        "python",
-        "bin/adversarial-score",
-        "--model",
-        "/tmp/model/pca-filtered-model-784-components-retraining",
-        "--eta",
-        "0.05",
-        "0.1",
-        "0.2",
-    ]
-    process = subprocess.run(command, stdout=subprocess.PIPE)
-    assert process.returncode == 0
-    adversarial_score_dictionary = json.loads(process.stdout)
-
-    assert len(adversarial_score_dictionary.keys()) == 3
-
-    expected = np.array([0.21, 0.61, 0.95])
     actual = np.array([adversarial_score_dictionary["0.05"],
                        adversarial_score_dictionary["0.1"],
                        adversarial_score_dictionary["0.2"]])
--- tests/test_train_model.py
+++ tests/test_train_model.py
@@ -30,26 +30,6 @@ def test_pca_when_reconstruction_is_saved(environ):
         assert os.path.exists(f)
 
 
-def test_pca_when_retraining_is_saved(environ):
-    command = ["python", "bin/train-model", "--retraining", "--epochs", "0",
-               "--pca", "--n-components", "100", "20"]
-    process = subprocess.run(command, stdout=subprocess.PIPE)
-    assert process.returncode == 0
-
-    created_files = [
-        "/tmp/model/pca-filtered-model-100-components-reconstruction/weights.h5",
-        "/tmp/model/pca-filtered-model-100-components-reconstruction/pca.pkl",
-        "/tmp/model/pca-filtered-model-20-components-reconstruction/weights.h5",
-        "/tmp/model/pca-filtered-model-20-components-reconstruction/pca.pkl",
-        "/tmp/model/pca-filtered-model-100-components-retraining/weights.h5",
-        "/tmp/model/pca-filtered-model-100-components-retraining/pca.pkl",
-        "/tmp/model/pca-filtered-model-20-components-retraining/weights.h5",
-        "/tmp/model/pca-filtered-model-20-components-retraining/pca.pkl",
-    ]
-    for f in created_files:
-        assert os.path.exists(f)
-
-
 def test_no_defense_network_is_saved(environ):
     command = ["python", "bin/train-model", "--epochs", "0"]
     process = subprocess.run(command, stdout=subprocess.PIPE)
@@ -61,23 +41,23 @@ def test_no_defense_network_is_saved(environ):
 def test_models_with_same_random_state_have_same_loss_and_accuracy(environ, mnist):
     X_train, _, X_test, y_test = mnist
     one_hot_y_test = keras.utils.to_categorical(y_test, 10)
-    command = ["python", "bin/train-model", "--retraining", "--pca",
+    command = ["python", "bin/train-model", "--pca",
                "--n-components", "20", "--epochs", "2"]
 
     process = subprocess.run(command, stdout=subprocess.PIPE)
     model = fc_100_100_10()
-    model.load_weights("/tmp/model/pca-filtered-model-20-components-retraining/weights.h5")
-    with open("/tmp/model/pca-filtered-model-20-components-retraining/pca.pkl", "rb") as f:
+    model.load_weights("/tmp/model/pca-filtered-model-20-components/weights.h5")
+    with open("/tmp/model/pca-filtered-model-20-components/pca.pkl", "rb") as f:
         pca = load(f)
     model = pca_filtered_model(model, X_train, pca=pca)
     expected = model.evaluate(X_test, one_hot_y_test)
 
-    shutil.rmtree("/tmp/model/pca-filtered-model-20-components-retraining/")
+    shutil.rmtree("/tmp/model/pca-filtered-model-20-components/")
 
     process = subprocess.run(command, stdout=subprocess.PIPE)
     model = fc_100_100_10()
-    model.load_weights("/tmp/model/pca-filtered-model-20-components-retraining/weights.h5")
-    with open("/tmp/model/pca-filtered-model-20-components-retraining/pca.pkl", "rb") as f:
+    model.load_weights("/tmp/model/pca-filtered-model-20-components/weights.h5")
+    with open("/tmp/model/pca-filtered-model-20-components/pca.pkl", "rb") as f:
         pca = load(f)
     model = pca_filtered_model(model, X_train, pca=pca)
     actual = model.evaluate(X_test, one_hot_y_test)
